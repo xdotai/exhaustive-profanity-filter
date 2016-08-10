ad2e1def96e65b932a5d0763c93a6b143f5efa3b
Kerem Kazan <kerem.kazan@human.x.ai>
2016-08-10 14:34:00 -0400

    basic structure done

---
+++ index.js
@@ -0,0 +1,45 @@
+'use strict'
+
+const fs = require('fs');
+const path = require('path');
+const _ = require('lodash');
+
+const badWordsDir = __dirname + '/data/filter/';
+const badWordsFiles = fs.readdirSync(badWordsDir);
+const badWordsWrapper = {
+  words: {}
+};
+
+badWordsFiles.forEach((badWordFile) => {
+  const badWordFilePath = path.join(badWordsDir, badWordFile);
+  const badWordsArray = fs.readFileSync(badWordFilePath)
+    .toString()
+    .split(",")
+    .map((badWord) => {
+      const replacedWord = badWord.replace(/[-_=\+\/\\\.<>\?\*;\(\)\{\}:\n]/g, '').trim();
+      return _.toLower(replacedWord);
+    });
+  const badWordsSet = new Set(badWordsArray);
+  badWordsSet.delete('');
+  if (badWordsSet.size > 0) {
+    const badWordsKey = badWordFile.split(".")[0];
+    badWordsWrapper.words[badWordsKey] = badWordsSet;
+  }
+});
+
+badWordsWrapper.contains = function(text) {
+  let isBadWord = false;
+  _.forEach(badWordsWrapper.words, (badWordsSet, badWordsKey) => {
+    const wordsInText = text.split(/[ ]+/);
+    _.forEach(wordsInText, (word) => {
+      if (badWordsSet.has(word)) {
+        isBadWord = true;
+        return;
+      }
+    });
+    if (isBadWord) return;
+  });
+  return isBadWord;
+};
+
+module.exports = badWordsWrapper;
